32202d921313b2199242820c154d52bb4fbaa7cf
beaterofficial01 <beaterofficial01@gmail.com>
2024-02-23 08:47:50 +0530

    Added check

--- Examples/check.js
+++
@@ -1,8 +0,0 @@
-(async function logMovies() {
-    const response = await fetch("https://randomuser.me/api");
-    const movies = await response.json();
-    const { first } = movies.results[0].name
-    const { last } = movies.results[0].name
-    const { title } = movies.results[0].name
-    console.log(title, first, last);
-})();
