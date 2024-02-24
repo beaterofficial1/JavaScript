a6f3f50aaae445e7f1359f95417caa95f1e57671
beaterofficial01 <beaterofficial01@gmail.com>
2024-02-25 01:33:10 +0530

    Final commit on 08_Events

--- 08_Events/script.js
+++ 08_Events/script.js
@@ -1,5 +1,28 @@
-const google = document.querySelector('#elephant')
-google.addEventListener('click', (e) => {
-    console.log(e.target.baseURI); // Events
-    // console.log(e.target);
-}, false)
+const images = document.querySelector('#images')
+const link = document.getElementById('google')
+
+
+// google.addEventListener('click', (e) => {
+//     // console.log(e.target.baseURI); // Events
+//     // e.target
+//     console.log(e);
+//     console.log('Button Clicked');
+// }, false) //event propogation & even bubbling
+
+
+// link.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     console.log('Google clicked');
+// }, false)
+
+images.addEventListener('click', (e) => {
+    if (e.target.tagName === 'IMG') {
+        let removeIt = e.target.parentNode
+        removeIt.remove()
+    }
+    // removeIt.remove()
+    // console.log(removeIt.parentNode);
+    // removeIt.parentNode.removeChild(removeIt)
+
+})
