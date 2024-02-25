6867b54dd7f482e5a502cf7dc285f8edff9672ae
beaterofficial01 <beaterofficial01@gmail.com>
2024-02-25 21:18:03 +0530

    .

--- Examples/random_color.js
+++
@@ -1,7 +0,0 @@
-function generateRandomHexCode() {
-    let randomNum = Math.floor(Math.random() * 16777215);
-    let hexCode = randomNum.toString(16).padStart(6, '0');
-    return '#' + hexCode;
-}
-
-console.log(generateRandomHexCode());
